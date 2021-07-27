128348b66eddb1bd3e215d1d04b46aa43f7225ec
MichaelPisciotta <mfp56@miami.edu>
2021-07-27 14:47:54 -0400

    done

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
 const dodger = document.getElementById("dodger"); 
 
-function moveDodgerLeft() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
-  
-    if (left > 0) {
-      dodger.style.left = `${left - 1}px`;
-    }
-  } 
-  
-  document.addEventListener("keydown", function(e) {
-    if (e.key === "ArrowLeft") {
-      moveDodgerLeft();
-    }
-  }); 
+//test 1 
+
+function moveDodgerLeft(){
+  const leftNumbers = dodger.style.left.replace("px", ""); // whatever we put in second arguement will replace what we put into the first  
+  const left = parseInt(leftNumbers, 10); //we need to change value of leftNumbers from a string to an integer to execute some kind of math
+
+
+  if (left > 0) {
+    dodger.style.left = `${left - 10}px`;
+  }
+}
+
+document.addEventListener("keydown", function(event) {
+  if (event.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+}); 
+
+//test 2 
+
 
 
   function moveDodgerRight() {
     const rightNumbers = dodger.style.left.replace("px", "");
-    const right = parseInt(rightNumbers, 10);
+    const left = parseInt(rightNumbers, 10);
   
-    if (right < 180) {
-      dodger.style.left = `${left + 1}px`;
+    if (left < 360) {
+      dodger.style.left = `${left + 10}px`;
     }
   } 
   
@@ -30,3 +36,52 @@ function moveDodgerLeft() {
       moveDodgerRight();
     }
   }); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const dodger = document.getElementById("dodger"); 
+
+// function moveDodgerLeft() {
+//     const leftNumbers = dodger.style.left.replace("px", "");
+//     const left = parseInt(leftNumbers, 10);
+  
+//     if (left > 0) {
+//       dodger.style.left = `${left - 1}px`;
+//     }
+//   } 
+  
+//   document.addEventListener("keydown", function(e) {
+//     if (e.key === "ArrowLeft") {
+//       moveDodgerLeft();
+//     }
+//   }); 
+
+
+
+
+//   function moveDodgerRight() {
+//     const rightNumbers = dodger.style.left.replace("px", "");
+//     const right = parseInt(rightNumbers, 10);
+  
+//     if (right < 180) {
+//       dodger.style.left = `${left + 1}px`;
+//     }
+//   } 
+  
+//   document.addEventListener("keydown", function(e) {
+//     if (e.key === "ArrowRight") {
+//       moveDodgerRight();
+//     }
+//   }); 
